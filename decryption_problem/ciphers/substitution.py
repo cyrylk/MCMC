c3ba42b56d7738cad8f7b05cba118d20817e1d42
Cyryl <cyryl28@gmail.com>
2020-08-14 17:11:54 +0200

    work on substitution code

--- decryption_problem/ciphers/substitution.py
+++ decryption_problem/ciphers/substitution.py
@@ -1,3 +1,6 @@
+#file for potential future development, currently it is not a part of the project
+
+
 import decryption_problem.alphabetic.alphabetic as alphabetic
 from random import choice, randint, random
 import decryption_problem.data.data_generator as data_generator
@@ -6,9 +9,6 @@ import decryption_problem.common.common as common
 from math import log
 import copy
 import decryption_problem.ciphers.vigenere as cipher
-import decryption_problem.algorithm.vigenere_neighbours as neighbours
-import decryption_problem.algorithm.vigenere_calculator as calculator
-import decryption_problem.algorithm.vigenere_decoder as decoder
 
 
 def create_stripped_encryption_decryption(text, encryption_decryption, alphabet):
@@ -135,42 +135,44 @@ def break_mcmc(encryption, alphabet, extended_alphabet, log_distribution, steps)
     return max_state
 
 
-def break_mcmc_double(encryption, alphabet, log_distribution, steps, length,
-                      monogram_log_distribution):
-    normal_alphabet = copy.deepcopy(alphabet)
-    current_guessed_encoding_key = alphabet
-    vigenere = decoder.get_max_monogram_state(encryption, monogram_log_distribution, length, alphabet)
-    current_decryption = cipher.encrypt_decrypt_text(encryption, vigenere, current_guessed_encoding_key)
-    current_frequencies = common.calculate_n_gram_frequencies(current_decryption, 2)
-    current_dist = common.calculate_n_gram_log_weight(current_frequencies, log_distribution)
-    max_dist = current_dist
-    max_state = copy.deepcopy(current_guessed_encoding_key)
-    for i in range(steps):
-        swap = get_random_swap(current_guessed_encoding_key)
-        swap_permutation(current_guessed_encoding_key, swap)
-        vigenere = decoder.get_max_monogram_state(encryption, monogram_log_distribution, length,
-                                                  current_guessed_encoding_key)
-        current_decryption = cipher.encrypt_decrypt_text(encryption, vigenere, current_guessed_encoding_key)
-        current_decryption = encrypt_decrypt_text(encryption, normal_alphabet, current_guessed_encoding_key, )
-        current_frequencies = common.calculate_n_gram_frequencies(cipher.encrypt_decrypt_text(current_decryption,
-                                                                                              vigenere,
-                                                                                              alphabet), 2)
-        dist_change = common.calculate_n_gram_log_weight(current_frequencies, log_distribution) - current_dist
-        if dist_change > log(random()):
-            current_dist += dist_change
-            if current_dist > max_dist:
-                max_dist = current_dist
-                max_state = copy.deepcopy(current_guessed_encoding_key)
-        else:
-            swap_permutation(current_guessed_encoding_key, (swap[1], swap[0]))
-    return max_state
+# def break_mcmc_double(encryption, alphabet, log_distribution, steps, length,
+#                       monogram_log_distribution):
+#     normal_alphabet = copy.deepcopy(alphabet)
+#     current_guessed_encoding_key = alphabet
+#     vigenere = decoder.get_max_monogram_state(encryption, monogram_log_distribution, length, alphabet)
+#     current_decryption = cipher.encrypt_decrypt_text(encryption, vigenere, current_guessed_encoding_key)
+#     current_frequencies = common.calculate_n_gram_frequencies(current_decryption, 2)
+#     current_dist = common.calculate_n_gram_log_weight(current_frequencies, log_distribution)
+#     max_dist = current_dist
+#     max_state = copy.deepcopy(current_guessed_encoding_key)
+#     for i in range(steps):
+#         swap = get_random_swap(current_guessed_encoding_key)
+#         swap_permutation(current_guessed_encoding_key, swap)
+#         vigenere = decoder.get_max_monogram_state(encryption, monogram_log_distribution, length,
+#                                                   current_guessed_encoding_key)
+#         current_decryption = cipher.encrypt_decrypt_text(encryption, vigenere, current_guessed_encoding_key)
+#         current_decryption = encrypt_decrypt_text(encryption, normal_alphabet, current_guessed_encoding_key, )
+#         current_frequencies = common.calculate_n_gram_frequencies(cipher.encrypt_decrypt_text(current_decryption,
+#                                                                                               vigenere,
+#                                                                                               alphabet), 2)
+#         dist_change = common.calculate_n_gram_log_weight(current_frequencies, log_distribution) - current_dist
+#         if dist_change > log(random()):
+#             current_dist += dist_change
+#             if current_dist > max_dist:
+#                 max_dist = current_dist
+#                 max_state = copy.deepcopy(current_guessed_encoding_key)
+#         else:
+#             swap_permutation(current_guessed_encoding_key, (swap[1], swap[0]))
+#     return max_state
 
 
-alphabet = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-plain = alphabetic.StrippedText(data_generator.generate_random_excerpt("../data/war_and_peace.txt", 1300).upper(),
+alphabet = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ")
+plain = alphabetic.StrippedText(
+    data_generator.get_string_cleared
+    (data_generator.generate_random_excerpt("../data/1984.txt", 10000)),
                                 alphabet)
 
-standard2 = data_generator.get_log_distribution_from_json("../data/bigram_log_distributions_uppercase.json")
+standard2 = data_generator.get_log_distribution_from_json("../data/bigram_log_distributions.json")
 standard1 = data_generator.get_log_distribution_from_json("../data/monogram_log_distributions_uppercase.json")
 
 all_printable = data_generator.get_string_cleared(string.printable + " ")
@@ -178,41 +180,18 @@ upper_printable = all_printable[:10] + all_printable[36:]
 complete_alphabet = alphabetic.Alphabet(data_generator.get_string_cleared(string.printable + " "))
 upper_alphabet = alphabetic.Alphabet(upper_printable)
 
-# print(plain.get_non_stripped_text())
-# freqs = common.calculate_n_gram_frequencies(plain, 2)
-# print(get_frequency_change(alphabet, ("A", "B"), upper_alphabet, freqs))
-# swap_permutation_and_update_decryption(plain, alphabet, ("A", "B"))
-# print(get_frequency_change(alphabet, ("B", "C"), upper_alphabet, freqs))
-# swap_permutation_and_update_decryption(plain, alphabet, ("B", "C"))
-# print(get_frequency_change(alphabet, ("C", "D"), upper_alphabet, freqs))
-
-# print(plain.get_non_stripped_text())
-# swap_permutation_and_update_decryption(plain, alphabet, ("B", "C"))
-# print(plain.get_non_stripped_text())
 permuted_alphabet = copy.deepcopy(alphabet)
 generate_random_permutation(permuted_alphabet)
 
 encryption = encrypt_decrypt_text(plain, permuted_alphabet, alphabet)
 print(encryption.get_non_stripped_text())
-# swap_permutation_and_update_decryption(encryption, alphabet, get_random_swap(alphabet))
-# print(encryption.get_non_stripped_text())
 
-another = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+another = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ")
 encryption2 = copy.deepcopy(encryption)
-x = break_mcmc(encryption, another, upper_alphabet, standard2, 25000)
+x = break_mcmc(encryption, another, complete_alphabet, standard2, 15000)
 
-another = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-# print(another.letters_to_position)
+another = alphabetic.Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ")
 print(encrypt_decrypt_text(encryption2, x, alphabet).get_non_stripped_text())
-
-encryption2 = copy.deepcopy(encryption)
-encryption2 = cipher.encrypt_decrypt_text(encryption2, [0,1,2,3,4,5,6,7,8,9], permuted_alphabet)
-y = break_mcmc_double(encryption2, another, standard2, 1000, 10, standard1)
-
-key = decoder.get_max_monogram_state(encryption2, standard1, 10, y)
-encryption3 = cipher.encrypt_decrypt_text(encryption2, key, y)
-encryption3 = encrypt_decrypt_text(encryption3, alphabet, y)
-print(encryption3.get_non_stripped_text())
 
 
 
